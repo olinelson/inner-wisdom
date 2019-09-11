46b0dddab2e99793294723cd113d561bb04673a6
olinelson <olivernelson@icloud.com>
2019-09-11 17:53:14 -0400

    added more alts

--- app/javascript/components/Memberships.jsx
+++ app/javascript/components/Memberships.jsx
@@ -6,7 +6,7 @@ export default function Memberships() {
         <Header as="h2" content="Proffesional Memberships" />
         <Item.Group >
             <Item>
-                <Item.Image size="tiny" src="https://storage.googleapis.com/inner_wisdom_bucket/APS_Member%20Logo.jpg" />
+                <Item.Image alt="APS Logo" size="tiny" src="https://storage.googleapis.com/inner_wisdom_bucket/APS_Member%20Logo.jpg" />
                 <Item.Content>
                     <h4>Australian Health Professionals Regulation Agency</h4>
                     <Item.Meta>Member Number 1137541</Item.Meta>
@@ -14,14 +14,14 @@ export default function Memberships() {
             </Item>
 
             <Item>
-                <Item.Image size="tiny" src="https://storage.googleapis.com/inner_wisdom_bucket/medicare.png" />
+                <Item.Image alt="medicare logo" size="tiny" src="https://storage.googleapis.com/inner_wisdom_bucket/medicare.png" />
                 <Item.Content>
                     <h4 >Medicare Provider</h4>
                     <Item.Meta>Provider Number 2577293T</Item.Meta>
                 </Item.Content>
             </Item>
             <Item>
-                <Item.Image size="tiny" src="https://storage.googleapis.com/inner_wisdom_bucket/logo_PsycBA.png" />
+                <Item.Image alt="Psychology Board of Australia logo" size="tiny" src="https://storage.googleapis.com/inner_wisdom_bucket/logo_PsycBA.png" />
                 <Item.Content>
                     <h4>Psychology Board of Australia</h4>
                     {/* <Item.Meta>Provider Number 2577293T</Item.Meta> */}
@@ -32,7 +32,7 @@ export default function Memberships() {
                 </Item.Content>
             </Item>
             <Item>
-                <Item.Image size="tiny" src="https://storage.googleapis.com/inner_wisdom_bucket/appheader-logo-mb-2x.jpg" />
+                <Item.Image alt="Fellow Australian Psychological Society logo" size="tiny" src="https://storage.googleapis.com/inner_wisdom_bucket/appheader-logo-mb-2x.jpg" />
                 <Item.Content>
                     <h4>Fellow Australian Psychological Society</h4>
                     {/* <Item.Meta>Provider Number 2577293T</Item.Meta> */}
