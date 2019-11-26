748e2f07881681cdd7432dac6be7e31398a8faed
Oli Nelson <olivernelson@icloud.com>
2019-11-26 12:11:36 +1100

    bug fix

--- app/javascript/components/ScheduleApp/Schedule.jsx
+++ app/javascript/components/ScheduleApp/Schedule.jsx
@@ -15,7 +15,7 @@ import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 import TimePicker from 'rc-time-picker';
 import "rc-time-picker/assets/index.css"
-import { Calendar as BigCalendar, momentLocalizer } from 'react-big-calendar'
+import { Calendar as BigCalendar, momentLocalizer, Views } from 'react-big-calendar'
 
 
 
